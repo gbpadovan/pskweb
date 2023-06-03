26bdb8bb40ff2e96a1c92f9ad76f670b5afdd991
gbpadovan <gustavo.padovan@protonmail.com>
2023-06-03 19:46:24 -0300

    other changes

--- psk/settings.py
+++ psk/settings.py
@@ -26,9 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+#ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['vercel.app']
 
 LOGIN_URL = '/entrar/'
 LOGIN_REDIRECT_URL = '/'
