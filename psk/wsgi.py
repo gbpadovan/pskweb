e1ec46a7070c6ca3036bc2b378696f4c82bad010
gbpadovan <gustavo.padovan@protonmail.com>
2023-06-04 20:17:23 -0300

    update wsgi.py

--- psk/wsgi.py
+++ psk/wsgi.py
@@ -1,10 +1,15 @@
 import os
+#from dotenv import load_dotenv
 
 from django.conf import settings
 from django.core.wsgi import get_wsgi_application
 
+#load_dotenv()
 
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "psk.settings")
+application = get_wsgi_application()
+app = application
+"""
 if settings.IN_PRODUCTION:
     # Vercel hosting:
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "psk.settings")
@@ -15,3 +20,4 @@ else:
     # use local host
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "psk.settings")
     application = get_wsgi_application()
+"""
