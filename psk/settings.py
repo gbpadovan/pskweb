bc780fe5688a881eb2080e627cf5bdb96636d8c2
gbpadovan <gustavo.padovan@protonmail.com>
2023-06-03 21:35:52 -0300

    changes in settings.py

--- psk/settings.py
+++ psk/settings.py
@@ -31,6 +31,7 @@ DEBUG = False
 #ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = ['vercel.app']
 
+# Login
 LOGIN_URL = '/entrar/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
@@ -138,6 +139,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media') #MEDIA_ROOT= BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# in production
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 
 # OTHER
